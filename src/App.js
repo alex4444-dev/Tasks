287640fe63c8db4b8b1dc50852f4b96abcace006
alex4444-dev <70803017+alex4444-dev@users.noreply.github.com>
2022-02-27 19:37:02 +0300

    HomeTask is performed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='parent'>
+      <div>
+        <h1>Добро пожаловать на мой сайт!</h1>
+      </div>
+      <div>
+        <p className='about'>
+          Что-то обо мне
+          <br></br>
+          <b> И <i>про мои </i>достижения!</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='header'>
+          <h2>Шапка</h2>
+        </div>
+        <div>
+          <hr></hr>
+        </div>
+        <div className='content'>
+          <h1>Контент</h1>
+        </div>
+        <div>
+          <hr></hr>
+        </div>
+        <div className='footer'>
+          <h2>Подвал</h2>
+        </div>
+      </div>
     </div>
   );
 }
