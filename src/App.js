f41bfbaedf135124f4ebd351c64cbeb9d0f693bc
alex4444-dev <70803017+alex4444-dev@users.noreply.github.com>
2022-02-28 23:56:35 +0300

    JavaScript to JSX hometask done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const first = 'React';
+  const second = 'Vue';
+  const third = 'Angular';
+  const fourth = 'Svelte';
+
+
+  const displayYear = () => {
+    return new Date().getFullYear()
+  }
+
+  
+
+  const protocol = 'https';
+  const domain = 'tproger.ru';
+  const path = 's3/uploads/2016/10/reactmini.png';
+  const alt = 'React-mini'
+
+  const avatar = 'https://edu.gntp.by/images/ava.png'
+
+  const person = {
+    name: 'Julia',
+    job: 'Postman'
+  }
+
+
   return (
-    <div className='parent'>
-      <div>
-        <h1>Добро пожаловать на мой сайт!</h1>
+    <div className="App">
+      <div className='firstTask'>
+        <h1>Ведущие Frontend технологии в {displayYear()} году</h1>
+        <ul>
+          <li>{first}</li>
+          <li>{second}</li>
+          <li>{third}</li>
+          <li>{fourth}</li>
+        </ul>
       </div>
+
+
       <div>
-        <p className='about'>
-          Что-то обо мне
-          <br></br>
-          <b> И <i>про мои </i>достижения!</b>
-        </p>
-        <div className='header'>
-          <h2>Шапка</h2>
-        </div>
-        <div>
-          <hr></hr>
-        </div>
-        <div className='content'>
-          <h1>Контент</h1>
-        </div>
-        <div>
-          <hr></hr>
-        </div>
-        <div className='footer'>
-          <h2>Подвал</h2>
-        </div>
+        <img src={`${protocol}://${domain}/${path}`} alt={alt} />
+        <h1>React JS нельзя освоить без JavaScript</h1>
       </div>
+
+      <div>
+        <h1>Work experience</h1>
+        <ul>
+          <li><strong>Name:</strong>{person.name}</li>
+          <li><strong>Job:</strong>{person.job}</li>
+          <li><strong>avatar:</strong></li>
+        </ul>
+        <img src={avatar}></img>
+      </div>
+
     </div>
   );
 }
